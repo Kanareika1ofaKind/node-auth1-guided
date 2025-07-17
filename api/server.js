8f6c0fcd80193b63e6fd9a5aeb2c0ae6f24da9ea
Sydney <proteimus@gmail.com>
2025-07-17 13:19:03 -0500

    Completed Auth1

--- api/server.js
+++ api/server.js
@@ -1,12 +1,39 @@
 const path = require('path')
 const express = require('express')
-const authRouter = require('./auth/auth-router')
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
+
+const authRouter = require('./auth/auth-router.js')
 const usersRouter = require('./users/users-router.js')
 
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../client')))
 server.use(express.json())
+server.use(session({
+    name: 'monkey',
+    secret: 'cookie monster',
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false, 
+        httpOnly: false,
+
+    },
+    rolling: true,
+    resave: false,
+    saveUninitialized: false,
+    srore: new Store({
+        knex: require('../database/db-config'),
+        tablename: 'session',
+        sidfieldname: 'sid',
+        createtable: true,
+        clearInterval: 1000 * 60 * 60,
+    })
+
+}))
+
+
+
 server.use('/api/auth', authRouter)
 server.use('/api/users', usersRouter)
 
